13613e2f684245849b031474327613f9edbcc899
coro <frantisek@barancik.net>
2019-01-27 23:37:07 +0100

    Add filters

--- Bulkgate/Customers.php
+++ Bulkgate/Customers.php
@@ -23,9 +23,116 @@ class Customers extends \BulkGate\Extensions\Customers
 		return $collection->getData();
 	}
 
+	private function getCondition($filter)
+	{
+		$cond = array();
+		foreach ($filter['values'] as $value) {
+			if (in_array($filter['type'], array('enum', 'string', 'float'), true)) {
+				if($value[0] === 'prefix')
+				{
+					$cond[] = array('like' => '%'.$value[1].'%');
+				}
+				elseif($value[0] === 'sufix')
+				{
+					$cond[] = array('like' => '%'.$value[1]);
+				}
+				elseif($value[0] === 'substring')
+				{
+					$cond[] = array('like' => '%'.$value[1].'%');
+				}
+				elseif($value[0] === 'empty')
+				{
+					$cond[] = array('eq' => $value[1]);
+				}
+				elseif($value[0] === 'filled')
+				{
+					$cond[] = array('neq' => $value[1]);
+				}
+				elseif($value[0] === 'is')
+				{
+					$cond[] = array('eq' => $value[1]);
+				}
+				elseif($value[0] === 'not')
+				{
+					$cond[] = array('neq' => $value[1]);
+				}
+				elseif($value[0] === 'gt')
+				{
+					$cond[] = array('gt' => $value[1]);
+				}
+				elseif($value[0] === 'lt')
+				{
+					$cond[] = array('lt' => $value[1]);
+				}
+			} elseif ($filter['type'] === "date-range") {
+				$cond[] = array(
+					'from' => date('Y-m-d H:i:s', strtotime($value[1])),
+					'to' => date('Y-m-d H:i:s', strtotime($value[2]))
+				);
+			}
+		}
+		return $cond;
+	}
+	
 	protected function filter(array $filters)
 	{
-		return '';
+		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+		$customers = array();
+		$filtered = false;
+		foreach ($filters as $key => $filter) {
+			if (isset($filter['values']) && count($filter['values']) > 0 && !$this->empty) {
+				/** @var \Magento\Customer\Model\ResourceModel\Customer\Collection $collection */
+				$collection = $objectManager->create(\Magento\Customer\Model\ResourceModel\Customer\Collection::class);
+				switch ($key) {
+					case 'firstname':
+						$collection->addFieldToFilter('firstname', $this->getCondition($filter));
+						foreach ($collection as $item) {
+							$customers[] = $item->getId();
+						}
+						break;
+					case 'lastname':
+						$collection->addFieldToFilter('lastname', $this->getCondition($filter));
+						foreach ($collection as $item) {
+							$customers[] = $item->getId();
+						}
+						break;
+					case 'country_id':
+						foreach ($filter['values'] as &$value) {
+							$value[1] = strtoupper($value[1]);
+						}
+						$collection->addNameToSelect()
+							->joinAttribute('billing_country_id', 'customer_address/country_id', 'default_billing', null, 'left')
+							->joinAttribute('shipping_country_id', 'customer_address/country_id', 'default_shipping', null, 'left');
+						$collection->getSelect()
+							->columns('IFNULL(`at_shipping_country_id`.`country_id`, `at_billing_country_id`.`country_id`) AS country_id');
+
+						$collection->getSelect()->having($this->getSql($filter, 'country_id'));
+//						$this->getSql($filter)
+						foreach ($collection as $item) {
+							$customers[] = $item->getId();
+						}
+						break;
+					case 'gender':
+						$collection->addFieldToFilter('gender', $this->getCondition($filter));
+						foreach ($collection as $item) {
+							$customers[] = $item->getId();
+						}
+						break;
+					case 'billing_city':
+						$collection
+							->joinAttribute('billing_city', 'customer_address/city', 'default_billing', null, 'left')
+							->addFieldToFilter('billing_city', $this->getCondition($filter));
+						foreach ($collection as $item) {
+							$customers[] = $item->getId();
+						}
+						break;
+				}
+			}
+			$filtered = true;
+
+		}
+
+		return array(array_unique($customers), $filtered);
 	}
 
 	protected function getTotal()
@@ -35,10 +142,14 @@ class Customers extends \BulkGate\Extensions\Customers
 
 	protected function getFilteredTotal(array $customers)
 	{
-		return '';
+//		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+		/** @var \Magento\Customer\Model\ResourceModel\Customer\Collection $collection */
+//		$collection = $objectManager->create(\Magento\Customer\Model\ResourceModel\Customer\Collection::class);
+//		return $collection->addFieldToFilter('entity_id', array('in' => $customers))->count();
+		return $this->getCustomerCollection($customers)->count();
 	}
 
-	public function getCustomerCollection(array $customers = array()) {
+	protected function getCustomerCollection(array $customers = array()) {
 		if ($this->cache && $this->customers === $customers) {
 			return $this->cache;
 		}
@@ -74,27 +185,14 @@ class Customers extends \BulkGate\Extensions\Customers
 		}
 		$collection->addFieldToFilter($filter);
 
-		/** @var Smsprofile $profile */
-/*		$profile = $objectManager->get(Smsprofile::class);
-		if ($profile->user->getPrefbilling()) {
-			if ($attr->getId())
-				$collection->getSelect()
-					->columns('IF(`at_shipping_telephone`.`telephone`, `at_shipping_telephone`.`telephone`, IF(`at_billing_telephone`.`telephone`, `at_billing_telephone`.`telephone`, `at_mobile`.`value`)) AS telephone');
-			else
-				$collection->getSelect()
-					->columns('IF(`at_shipping_telephone`.`telephone`, `at_shipping_telephone`.`telephone`, `at_billing_telephone`.`telephone`) AS telephone');
+		if ($attr->getId())
 			$collection->getSelect()
-				->columns('IF(`at_billing_country_id`.`country_id`, `at_billing_country_id`.`country_id`, `at_shipping_country_id`.`country_id`) AS country_id');
-		} else {*/
-			if ($attr->getId())
-				$collection->getSelect()
-					->columns('IF(`at_billing_telephone`.`telephone`, `at_billing_telephone`.`telephone`, IF(`at_shipping_telephone`.`telephone`, `at_shipping_telephone`.`telephone`, `at_mobile`.`value` )) AS telephone');
-			else
-				$collection->getSelect()
-					->columns('IF(`at_billing_telephone`.`telephone`, `at_billing_telephone`.`telephone`, `at_shipping_telephone`.`telephone`) AS telephone');
+				->columns('IF(`at_billing_telephone`.`telephone`, `at_billing_telephone`.`telephone`, IF(`at_shipping_telephone`.`telephone`, `at_shipping_telephone`.`telephone`, `at_mobile`.`value` )) AS telephone');
+		else
 			$collection->getSelect()
-				->columns('IF(`at_shipping_country_id`.`country_id`, `at_shipping_country_id`.`country_id`, `at_billing_country_id`.`country_id`) AS country_id');
-//		}
+				->columns('IF(`at_billing_telephone`.`telephone`, `at_billing_telephone`.`telephone`, `at_shipping_telephone`.`telephone`) AS telephone');
+		$collection->getSelect()
+			->columns('IF(`at_shipping_country_id`.`country_id`, `at_shipping_country_id`.`country_id`, `at_billing_country_id`.`country_id`) AS country_id');
 
 		if ($customers) {
 			$collection->addFieldToFilter('entity_id', array('in' => $customers));
@@ -105,23 +203,6 @@ class Customers extends \BulkGate\Extensions\Customers
 			->columns('e.lastname AS last_name');
 
 		return $this->cache = $collection;
-/*		return $this->db->execute("
-            SELECT      `user_id` AS `order`,
-                        MAX(CASE WHEN meta_key = 'billing_first_name' AND meta_value IS NOT NULL THEN meta_value ELSE (CASE WHEN meta_key = 'first_name' THEN  meta_value ELSE (CASE WHEN meta_key = 'shipping_first_name' THEN  meta_value END) END) END) first_name,
-                        MAX(CASE WHEN meta_key = 'billing_last_name' AND meta_value IS NOT NULL THEN meta_value ELSE (CASE WHEN meta_key = 'last_name' THEN  meta_value ELSE (CASE WHEN meta_key = 'shipping_last_name' THEN  meta_value END)  END) END) last_name,
-                        MAX(CASE WHEN meta_key = 'billing_phone' THEN meta_value END) phone_mobile,
-                        MAX(CASE WHEN meta_key = 'billing_company' THEN meta_value ELSE (CASE WHEN meta_key = 'shipping_company' THEN  meta_value END) END) company_name,
-                        MAX(CASE WHEN meta_key = 'billing_country' THEN LOWER(meta_value) ELSE (CASE WHEN meta_key = 'shipping_country' THEN  LOWER(meta_value) END)  END) country,
-                        MAX(CASE WHEN meta_key = 'billing_address_1' THEN meta_value ELSE (CASE WHEN meta_key = 'shipping_address_1' THEN  meta_value END) END) street1,
-                        MAX(CASE WHEN meta_key = 'billing_address_2' THEN meta_value ELSE (CASE WHEN meta_key = 'shipping_address_2' THEN  meta_value END) END) street2,
-                        MAX(CASE WHEN meta_key = 'billing_postcode' THEN meta_value ELSE (CASE WHEN meta_key = 'shipping_postcode' THEN  meta_value END) END) zip,
-                        MAX(CASE WHEN meta_key = 'billing_city' THEN meta_value ELSE (CASE WHEN meta_key = 'shipping_city' THEN  meta_value END) END) city,
-                        MAX(CASE WHEN meta_key = 'billing_email' THEN meta_value END) email
-            FROM `{$this->db->table('usermeta')}`
-            ". (count($customers) > 0 ? "WHERE `user_id` IN ('".implode("','", $customers)."') " : "") . "
-            GROUP BY `user_id`
-            HAVING `phone_mobile` NOT LIKE '' 	
-            ". ($limit !== null ? "LIMIT $limit" : ""))->getRows();*/
 	}
 
 
